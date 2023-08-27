72b4ad6e9962f3d49c2dc969d6e0b3fbb34dfef0
Ivan Marchenko <vanmaru.kun@gmail.com>
2023-08-27 21:55:17 +0300

    DishRecipe command added, small refactoring

--- WhatToCookWithIt/Commands/DishListCommand.cs
+++ WhatToCookWithIt/Commands/DishListCommand.cs
@@ -4,41 +4,43 @@ using WhatToCookWithIt.Entities;
 using WhatToCookWithIt.Interfaces;
 using Newtonsoft.Json;
 using System.Text;
+using Telegram.Bot.Types.Enums;
 
 namespace WhatToCookWithIt.Commands
 {
     public class DishListCommand : ICommand
     {
         public TelegramBotClient Client => Bot.GetTelegramBot();
-
         public string Name => "/DishList";
-
         public async Task Execute(Update update)
         {
             long chatId = update.Message.Chat.Id;
             string messageText = update.Message.Text;
+
             string[] commandParts = messageText.Split(' ');
+
             if (commandParts.Length > 1)
             {
                 string ingredient = string.Join(" ", commandParts.Skip(1));
-
                 string apiUrl = $"https://www.themealdb.com/api/json/v1/1/filter.php?i={ingredient}";
-
                 using (HttpClient httpClient = new HttpClient())
                 {
                     HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
                     string jsonContent = await response.Content.ReadAsStringAsync();
-
                     var result = JsonConvert.DeserializeObject<ApiResult>(jsonContent);
                     if (result != null && result.Meals != null && result.Meals.Any())
                     {
                         StringBuilder messageBuilder = new StringBuilder();
                         messageBuilder.AppendLine($"Блюда с ингредиентом '{ingredient}':");
+
                         foreach (var meal in result.Meals)
                         {
-                            messageBuilder.AppendLine($"- {meal.StrMeal}");
+                            string recipeLink = $"/DishRecipe {meal.IdMeal}";
+                            string clickableMealName = $"[{meal.StrMeal}]({recipeLink})";
+
+                            messageBuilder.AppendLine(clickableMealName);
                         }
-                        await Client.SendTextMessageAsync(chatId, messageBuilder.ToString());
+                        await Client.SendTextMessageAsync(chatId, messageBuilder.ToString(), parseMode: ParseMode.Markdown);
                     }
                     else
                     {
@@ -58,6 +60,9 @@ namespace WhatToCookWithIt.Commands
         }
         private class Meal
         {
+            [JsonProperty("idMeal")]
+            public string IdMeal { get; set; }
+
             [JsonProperty("strMeal")]
             public string StrMeal { get; set; }
         }
---
+++ WhatToCookWithIt/Commands/DishRecipeCommand.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using WhatToCookWithIt.Entities;
+using WhatToCookWithIt.Interfaces;
+
+namespace WhatToCookWithIt.Commands
+{
+    public class DishRecipeCommand : ICommand
+    {
+        public TelegramBotClient Client => Bot.GetTelegramBot();
+        public string Name => "/DishRecipe";
+        public async Task Execute(Update update)
+        {
+            long chatId = update.Message.Chat.Id;
+            string messageText = update.Message.Text;
+            string[] commandParts = messageText.Split(' ');
+
+            if (commandParts.Length > 1)
+            {
+                string dishId = commandParts[1];
+                string apiUrl = $"https://www.themealdb.com/api/json/v1/1/lookup.php?i={dishId}";
+
+                // Отправляем GET-запрос и получаем JSON-ответ
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                    string jsonContent = await response.Content.ReadAsStringAsync();
+
+                    // Распарсим JSON-ответ
+                    var result = JsonConvert.DeserializeObject<ApiResult>(jsonContent);
+
+                    if (result != null && result.Meals != null && result.Meals.Any())
+                    {
+                        var meal = result.Meals[0];
+                        // Формируем сообщение с рецептом
+                        string recipeMessage = $"Рецепт для блюда '{meal.StrMeal}':\n\n{meal.StrInstructions}";
+
+                        await Client.SendTextMessageAsync(chatId, recipeMessage);
+                    }
+                    else
+                    {
+                        await Client.SendTextMessageAsync(chatId, $"Не удалось найти рецепт для блюда с ID '{dishId}'.");
+                    }
+                }
+            }
+            else
+            {
+                await Client.SendTextMessageAsync(chatId, "Пожалуйста, укажите ID блюда.");
+            }
+        }
+
+        private class ApiResult
+        {
+            [JsonProperty("meals")]
+            public List<Meal> Meals { get; set; }
+        }
+        private class Meal
+        {
+            [JsonProperty("strMeal")]
+            public string StrMeal { get; set; }
+
+            [JsonProperty("strInstructions")]
+            public string StrInstructions { get; set; }
+        }
+    }
+}
